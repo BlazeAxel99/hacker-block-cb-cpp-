627251a66a202297279df774ab9184297d08df8e
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-08-21 21:05:30 +0530

    Add files via upload

---
+++ pattern .cpp
@@ -0,0 +1,33 @@
+/*
+                 1
+                232
+               34543
+              4567654
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+for(int i=1;i<=n;i++){
+
+
+for(int space=1;space<=n-i;space++){
+    cout<<" ";
+}
+//Write your code here
+int val=i;
+for(int cnt=1;cnt<=i;cnt++){
+cout<<val;
+val=val + 1;
+
+}
+val= val-2 ;
+for(int cnt=1;cnt<=i-1;cnt++){
+cout<<val;
+val=val-1;
+}
+cout<<endl;
+}
+}
