c477b8e509b182e7b4ce8240b2eabb8011cdfbf3
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-08-25 11:42:57 +0530

    Add files via upload

---
+++ scoperesolutionoperator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int x=100;
+int main(){
+int x= 10;
+cout<<x<<endl;
+for(x= 0;x<=5;x++){
+cout<<x<<endl;
+
+}//random no. 5 taken for testing and understanding the special op.
+
+cout<<x<<endl;
+cout<< ::x<<endl;
+}
+//if we add int in the for loop x output changes.
