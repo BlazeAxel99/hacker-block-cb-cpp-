09a2399a064bb6a40259ba8dd8fe5938ad0a468f
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-08-25 11:53:02 +0530

    Add files via upload

---
+++ cin.get().cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+char ch;
+//cin>>ch; will not take space as input it wil joint the two statment
+ch=cin.get();
+
+while(ch!='.'){
+cout<<ch;
+ch=cin.get();
+
+
+}
+
+
+//Write your code here
+
+}
