096ad14dd135c91666832ff5c4b5e39dc713abaf
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-09-01 11:08:39 +0530

    Add files via upload

---
+++ p2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+//Write your code here
+int n;
+cin>>n;
+for(int i=1;i<=n;i++){
+int val=i%2==0?1:0;
+for(int cnt=1;cnt<=i;cnt++){
+cout<<val;
+val=1-val;
+}
+cout<<endl;
+}
+return 0;
+}
