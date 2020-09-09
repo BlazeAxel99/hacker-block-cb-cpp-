27979fca78d78c3ca53896d679d5326cff9e856d
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-09-09 20:09:46 +0530

    Add files via upload

---
+++ trailing zero.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+
+long long int n,fact=1,res,p=1,sum=0;
+cin>>n;
+
+/*for(int i=2;i<=n;i++){
+fact*=i;
+
+}
+cout<<fact<<endl;
+*/
+//Write your code here
+for(int j=1;j<=n;j++){
+res=pow(5,j);
+p=n/res;
+sum=sum+p;
+if(p<0){
+
+break;
+}
+
+}
+cout<<sum;
+
+}
---
+++ trailing zeros eff.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void trail(int n){
+int ans=0;
+for(int D=5; n/D>=1;D*=5){
+ans+=n/D;
+}
+cout<<ans;
+
+}
+
+int main(){
+
+long long int n;
+cin>>n;
+trail(n);
+//Write your code here
+
+}
