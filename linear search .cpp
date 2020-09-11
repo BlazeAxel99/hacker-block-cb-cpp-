32a646951fd5590c6298e52917fa6563db98eef7
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-09-11 20:12:27 +0530

    Add files via upload

---
+++ linear search .cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+
+void ls(int n){
+int a[1000];
+int key;
+
+for(int i=0;i<=n-1;i++){
+cin>>a[i];
+
+}
+cin>>key;
+for (int i=0;i<=n;i++){
+if(a[i]==key) {
+
+cout<<i;
+break;
+}
+else if(i!=n){
+continue;
+}
+else if(i==n){
+cout<<-1;
+
+}
+
+}
+
+}
+
+
+
+int main(){
+
+int n;
+cin>>n;
+ls(n);
+//Write your code here
+
+}
