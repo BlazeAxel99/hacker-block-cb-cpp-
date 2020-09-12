488c4a410a2674a13cd56d1d1b2fdb57320c162c
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-09-12 11:45:12 +0530

    Add files via upload

---
+++ inbuilt sort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+// use comp for dec order else remove it from the code.
+bool comp(int a,int b){
+return a>b;
+
+}
+
+int main(){
+//Write your code here
+int n, a[1000];
+
+cin>>n;
+for(int i=0;i<n;i++){
+
+cin>>a[i];
+}
+sort(a,a+n,comp);
+for(int i=0;i<n;i++){
+
+cout<<a[i]<<",";
+}
+
+return 0;
+}
