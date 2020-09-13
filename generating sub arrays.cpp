4033b010914f9d287202d2044af7c8b9582975b8
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-09-13 12:48:12 +0530

    Add files via upload

---
+++ generating sub arrays.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a[1000],n;
+cin>>n;
+for(int i=0;i<n;i++){
+cin>>a[i];
+}
+//Write your code here
+for(int i=0;i<n;i++){
+for(int j=i;j<n;j++){
+for(int k=i;k<=j;k++){
+cout<<a[k]<<",";
+}
+cout<<endl;
+}
+}
+return 0;
+}
