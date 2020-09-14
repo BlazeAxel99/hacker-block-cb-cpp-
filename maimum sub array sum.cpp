74a1968b0c0a0a8b44d030db4d9bdc6714147fd0
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-09-14 10:39:28 +0530

    Add files via upload

---
+++ maimum sub array sum.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a[1000],n,csum=0,msum=0,left,right;
+cin>>n;
+for(int i=0;i<n;i++){
+cin>>a[i];
+}
+//Write your code here
+for(int i=0;i<n;i++){
+for(int j=i;j<n;j++){
+csum=0;
+for(int k=i;k<=j;k++){
+cout<<a[k]<<",";
+csum+=a[k];
+
+}
+if(csum>msum){
+msum=csum;
+left=i;
+right=j;
+}
+cout<<endl;
+}
+}
+for(int k=left;k<=right;k++){
+cout<<a[k]<<","<<endl;
+}
+cout<<"max sum:"<<msum;
+return 0;
+}
