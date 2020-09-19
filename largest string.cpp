9dd531cc607ac03f49151cbfc17f1c1ae0b9170f
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-09-19 11:04:51 +0530

    Add files via upload

---
+++ largest string.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+//Write your code here
+int n;
+
+cin>>n;
+char a[1000];
+char largest[1000];
+
+int len=0;
+int largest_len=0;
+
+cin.get();
+
+for(int i=0;i<n;i++){
+
+cin.getline(a,1000);
+//cout<<a<<endl;
+
+len= strlen(a);
+if(len>largest_len){
+largest_len=len;
+strcpy(largest,a);
+
+}
+
+}
+
+cout<<largest;
+
+
+
+
+return 0;
+}
