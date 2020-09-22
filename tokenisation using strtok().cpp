6a0b8cea4ca4828af540575a275748975a057490
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2020-09-22 10:57:30 +0530

    Add files via upload

---
+++ tokenisation using strtok().cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+int main(){
+//Write your code here
+char s[100]="Today is a rainy day";
+
+char *ptr = strtok(s," ");
+cout<<ptr<<endl;
+while(ptr!=NULL){
+ ptr=strtok(NULL," ");
+ cout<<ptr<<endl;
+
+}
+
+return 0;
+}
