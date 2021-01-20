fc2d6b2e5404edbd3086d6db249a2085e1c3437c
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2021-01-20 12:53:07 +0530

    Create   💡 Print LCS.cpp

---
+++ 💡 Print LCS.cpp
@@ -0,0 +1,28 @@
+/*
+
+
+A subsequence is a sequence that can be derived from another sequence by deleting some elements without changing the order of the remaining elements. For example, the sequence {A,B,D} is a subsequence of {A,B,C,D,E,F}, obtained after removal of elements C, E and F.
+
+Given two strings A and B of size n and m respectively, you have to print the Longest Common Subsequence(LCS) of strings A and B, where LCS is the longest sequence present in both A and B.
+
+Note: It is gauranteed that there is only one unique longest common subsequence
+Input Format
+
+Two strings A and B.
+Constraints
+
+1 <= n,m <= 10^3
+Output Format
+
+Output the LCS of A and B.
+Sample Input
+
+abc
+acd
+
+Sample Output
+
+ac
+
+*/
+
